01ed4a9b6b52af5a0354fb2eaa608c8c1271305c
SSaif-13 <sadat.saif@ucalgary.ca>
2025-04-19 02:03:03 -0600

    Implemented login frontend

--- Carbon-Wakeup/src/Login.jsx
+++ Carbon-Wakeup/src/Login.jsx
@@ -1,38 +1,51 @@
-import React from 'react';
-// import './Login.css'; need to create this
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import './Login.css'; // make sure to create and customize this
 
 const Login = () => {
-  return (
-    <>
-      <div className="Login-container">
-        {/* Header */}
-        <div>
-          <div className="header">
-            <h1 className="navbar-brand">Login</h1>
-            <div className="navigation">
-              <ul className="unordered-list">
-                <li>
-                  <Link to="/">Homepage</Link>
-                </li>
-                <li>
-                  <Link to="/about">About Us</Link>
-                </li>
-                <li> <Link to="/compare"> Compare Carbon Emissions </Link> </li>
-                <li>
-                  <Link to="/calculator">Carbon Footprint Calculator</Link>
-                </li>
-                <li>
-                  <Link to="/donate">Donate</Link>
-                </li>
+  const [signIn, setSignIn] = useState(true);
 
-              </ul>
-            </div>
-          </div>
+  return (
+    <div className="login-page">
+      {/* Header/Navbar */}
+      <div className="header">
+        <h1 className="navbar-brand">Login</h1>
+        <nav className="navigation">
+          <ul className="nav-list">
+            <li><Link to="/">Homepage</Link></li>
+            <li><Link to="/about">About Us</Link></li>
+            <li><Link to="/compare">Compare Carbon Emissions</Link></li>
+            <li><Link to="/calculator">Carbon Footprint Calculator</Link></li>
+            <li><Link to="/donate">Donate</Link></li>
+          </ul>
+        </nav>
+      </div>
+
+      {/* Login/Signup container */}
+      <div className={`login-signup-container ${signIn ? 'sign-in-mode' : 'sign-up-mode'}`}>
+        {/* Sign Up Form */}
+        <div className="signup-form-container">
+          <form className="signup-form">
+            <h1>Create Account</h1>
+            <input className="signup-input" type="text" placeholder="Name" />
+            <input className="signup-input" type="email" placeholder="Email" />
+            <input className="signup-input" type="password" placeholder="Password" />
+            <button className="signup-button">Sign Up</button>
+          </form>
+        </div>
+
+        {/* Sign In Form */}
+        <div className="login-form-container">
+          <form className="login-form">
+            <h1>Sign in</h1>
+            <input className="login-input" type="email" placeholder="Email" />
+            <input className="login-input" type="password" placeholder="Password" />
+            <a href="#" className="login-forgot-password">Forgot your password?</a>
+            <button className="login-button">Sign In</button>
+          </form>
         </div>
       </div>
-    </>
-
+    </div>
   );
 };
 
